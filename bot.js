34abed9205cd7f3e25e86264a9445bee601b7c4b
joinity <18332291+joinity@users.noreply.github.com>
2019-05-02 00:25:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,12 +9,11 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 	if (!msg.content.startsWith("!") || msg.author.bot) return;
-	console.log(msg);
 	var pkmn = msg.content.slice(1).split(/ +/);
-	console.log(pkmn);
 	pkmn = pkmn.toString().toLowerCase();
 	console.log(pkmn);
 	for(var i=0;i<pokemon.length;i++){
+			console.log(pokemon[i].Name.toLowerCase());
 			if(pkmn == pokemon[i].Name.toLowerCase()){
 				console.log('I am here');
 				var text = pokemon[i].Name;
