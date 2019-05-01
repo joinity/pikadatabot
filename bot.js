a9c0432275bd279a03050a872ec6910bc7897a3d
joinity <18332291+joinity@users.noreply.github.com>
2019-05-02 00:13:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,17 +7,40 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-	if (!message.content.startsWith("!") || message.author.bot) return;
+bot.on("message", msg => {
+	let prefix = "!";
+	if (!msg.content.startsWith(prefix)) return;
+
+	if(msg.author.bot) return;
+
+    if (msg.content == prefix+"ping") {
+        msg.channel.sendMessage("!pong");
+    }
+    else if (msg.content == prefix+"<3"){
+        msg.channel.sendMessage("I love you, Master!");
+    }
 	
-	//var pkmn = message.content.toString().toLowerCase();
-	//pkmn = pkmn.substring(1);
-	message.reply("hi");
-	/*for(var i=0;i<pokemon.length;i++){
-			if(pkmn == pokemon[i].Name.toLowerCase()){
-				var text = pokemon[i].Name;
-				message.reply(text);
+	else if (msg.content.startsWith(prefix+"pokemon")){
+		var pkmn = msg.content.toString().toLowerCase();
+		pkmn = pkmn.substring(9);
+		for(var i=0;i<pokemon.length;i++){
+			if(pkmn == pokemon[i]._engName.toLowerCase() || pkmn == pokemon[i]._frName.toLowerCase() || pkmn == pokemon[i]._nb){
+				var text = "__You selected__\n";
+				text += "**N° "+pokemon[i]._nb+"** \n";
+				text += "English name: **"+pokemon[i]._engName+"** \n";
+	 			text += "French name: **"+pokemon[i]._frName+"** \n";
+				text += "Type: **"+pokemon[i]._type+" "+pokemon[i]._type2+"**\n";
+				text += "Catch Ratio: **"+pokemon[i]._catchRate+"**\n\n";
+				text += "__Base Stats__\n";
+				text += "HP: **"+pokemon[i]._baseStats._hp+"**\n";
+				text += "Atk: **"+pokemon[i]._baseStats._atk+"**\n";
+				text += "Def: **"+pokemon[i]._baseStats._def+"**\n";
+				text += "S. Atk: **"+pokemon[i]._baseStats._sAtk+"**\n";
+				text += "S. Def: **"+pokemon[i]._baseStats._sDef+"**\n";
+				text += "Spd: **"+pokemon[i]._baseStats._spd+"**";
+				msg.reply(text);
 			}
-		
-	}*/
+		}
+	}
 });
+
