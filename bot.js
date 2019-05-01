8c9ae5f9d9acd89caf66c22ff42fa079f738c6bf
joinity <18332291+joinity@users.noreply.github.com>
2019-05-02 00:10:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
-//var pokemon = require('./pokedex');
+var pokemon = require('./pokedex');
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -10,8 +10,8 @@ client.on('ready', () => {
 client.on('message', message => {
 	if (!message.content.startsWith("!") || message.author.bot) return;
 	
-	var pkmn = message.content.toString().toLowerCase();
-	pkmn = pkmn.substring(1);
+	//var pkmn = message.content.toString().toLowerCase();
+	//pkmn = pkmn.substring(1);
 	message.reply("hi");
 	/*for(var i=0;i<pokemon.length;i++){
 			if(pkmn == pokemon[i].Name.toLowerCase()){
