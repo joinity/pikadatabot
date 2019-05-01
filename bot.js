2cdd3d590fb5981e18633c9b7d8e5cfd5b954421
joinity <18332291+joinity@users.noreply.github.com>
2019-05-02 00:09:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
-var pokemon = require('./pokedex');
+//var pokemon = require('./pokedex');
 
 client.on('ready', () => {
     console.log('I am ready!');
