d1661ef26641d44a5a9efab7e92503588e093292
joinity <18332291+joinity@users.noreply.github.com>
2019-05-02 18:14:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,10 +20,11 @@ client.on('message', msg => {
 					const embed = new Discord.RichEmbed()
 					.setAuthor(pokemon[i].Name, "https://img.pokemondb.net/sprites/black-white/normal/"+ pkmn +".png" )
 					.setColor(0xFF0000)
-					.addField("Stats", "Level" + pokemon[i].Level)
-					.addField("\u200B","⧫ ATK: " + pokemon[i].Damage + "\r\n ⧫ HP: " + pokemon[i].Health + "\r\n ⧫ DEF: " + pokemon[i].Defense, true)
-					.addField("\u200B", "⧫ RANGE: " + pokemon[i].Range+ "\r\n ⧫ DPS: " + pokemon[i].DPS + "\r\n ⧫ MOVE SPEED: " + pokemon[i].MoveSpeed, true)
-					.addField("Skills", pokemon[i].Ability)
+					.addField("Stats", "Level: " + pokemon[i].Level)
+					.addField("\u200B","Attack: " + pokemon[i].Damage + "\r\n HP: " + pokemon[i].Health + "\r\n Defense: " + pokemon[i].Defense, true)
+					.addField("\u200B", "Range: " + pokemon[i].Range+ "\r\n DPS: " + pokemon[i].DPS + "\r\n Move Speed: " + pokemon[i].MoveSpeed, true)
+					.addField("\u200B", "Cost: " + pokemon[i].Cost+ "\r\n Total Cost: " + pokemon[i].TotalCost + "\r\n Catch Rate: " + pokemon[i].CatchRate, true)
+					.addField("Skill:", pokemon[i].Ability)
 					.setThumbnail("https://img.pokemondb.net/sprites/black-white/anim/normal/"+ pkmn +".gif");
 					msg.author.send({embed});
 
