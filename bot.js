1c692b3ac3cb61b0e3ae7aba6fe98a2041429e27
joinity <18332291+joinity@users.noreply.github.com>
2019-05-02 15:35:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,26 +8,50 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-	if (!msg.content.startsWith("!") || msg.author.bot) return;
+	
+	if (msg.content.startsWith("!"){
 	var pkmn = msg.content.slice(1).split(/ +/);
 	pkmn = pkmn.toString().toLowerCase();
 	console.log(pkmn);
-	for(var i=0;i<pokemon.length;i++){
-			
-			if(pkmn == pokemon[i].Name.toLowerCase()){
-				
-				const embed = new Discord.RichEmbed()
-     				.setAuthor(pokemon[i].Name, "https://img.pokemondb.net/sprites/black-white/normal/"+ pkmn +".png" )
-      				.setColor(0xFF0000)
-      				.addField("Stats", "Level" + pokemon[i].Level)
-				.addField("\u200B","⧫ ATK: " + pokemon[i].Damage + "\r\n ⧫ HP: " + pokemon[i].Health + "\r\n ⧫ DEF: " + pokemon[i].Defense, true)
-      				.addField("\u200B", "⧫ RANGE: " + pokemon[i].Range+ "\r\n ⧫ DPS: " + pokemon[i].DPS + "\r\n ⧫ MOVE SPEED: " + pokemon[i].MoveSpeed, true)
-      				.addField("Skills", pokemon[i].Ability)
-    				.setThumbnail("https://img.pokemondb.net/sprites/black-white/anim/normal/"+ pkmn +".gif");
-				msg.author.send({embed});
+		for(var i=0;i<pokemon.length;i++){
 
-			}
-		
+				if(pkmn == pokemon[i].Name.toLowerCase()){
+
+					const embed = new Discord.RichEmbed()
+					.setAuthor(pokemon[i].Name, "https://img.pokemondb.net/sprites/black-white/normal/"+ pkmn +".png" )
+					.setColor(0xFF0000)
+					.addField("Stats", "Level" + pokemon[i].Level)
+					.addField("\u200B","⧫ ATK: " + pokemon[i].Damage + "\r\n ⧫ HP: " + pokemon[i].Health + "\r\n ⧫ DEF: " + pokemon[i].Defense, true)
+					.addField("\u200B", "⧫ RANGE: " + pokemon[i].Range+ "\r\n ⧫ DPS: " + pokemon[i].DPS + "\r\n ⧫ MOVE SPEED: " + pokemon[i].MoveSpeed, true)
+					.addField("Skills", pokemon[i].Ability)
+					.setThumbnail("https://img.pokemondb.net/sprites/black-white/anim/normal/"+ pkmn +".gif");
+					msg.author.send({embed});
+
+				}
+
+		}
+	}
+	else if (msg.content.startsWith("?")){
+	var pkmn = msg.content.slice(1).split(/ +/);
+	pkmn = pkmn.toString().toLowerCase();
+	console.log(pkmn);
+		for(var i=0;i<pokemon.length;i++){
+
+				if(pkmn == pokemon[i].Name.toLowerCase()){
+
+					const embed = new Discord.RichEmbed()
+					.setAuthor(pokemon[i].Name, "https://img.pokemondb.net/sprites/black-white/normal/"+ pkmn +".png" )
+					.setColor(0xFF0000)
+					.addField("Stats", "Level" + pokemon[i].Level)
+					.addField("\u200B","⧫ ATK: " + pokemon[i].Damage + "\r\n ⧫ HP: " + pokemon[i].Health + "\r\n ⧫ DEF: " + pokemon[i].Defense, true)
+					.addField("\u200B", "⧫ RANGE: " + pokemon[i].Range+ "\r\n ⧫ DPS: " + pokemon[i].DPS + "\r\n ⧫ MOVE SPEED: " + pokemon[i].MoveSpeed, true)
+					.addField("Skills", pokemon[i].Ability)
+					.setThumbnail("https://img.pokemondb.net/sprites/black-white/anim/normal/"+ pkmn +".gif");
+					msg.channel.send({embed});
+
+				}
+
+		}
 	}
 	
 });
