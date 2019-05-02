3dea0359c0064931cb3cac93db6040c893d7aa10
joinity <18332291+joinity@users.noreply.github.com>
2019-05-02 14:19:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,23 +16,11 @@ client.on('message', msg => {
 			
 			if(pkmn == pokemon[i].Name.toLowerCase()){
 				
-const exampleEmbed = new Discord.RichEmbed()
-	.setColor('#0099ff')
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.addField('Regular field title', 'Some value here')
-	.addBlankField()
-	.addField('Inline field title', 'Some value here', true)
-	.addField('Inline field title', 'Some value here', true)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-
-msg.channel.send(exampleEmbed);
+				const exampleEmbed = new Discord.RichEmbed()
+				.setColor('#0099ff')
+				.setTitle('Some title');
+				msg.channel.send(exampleEmbed);
+				msg.channel.send("WIP");
 				/*
      				.setAuthor(pokemon[i].Name)
       				.setColor(0xFF0000)
@@ -40,8 +28,8 @@ msg.channel.send(exampleEmbed);
       				.addField("\u200B", " RANGE: " + pokemon[i].Range+ "\r\n ⧫ DPS: " + pokemon[i].DPS + "\r\n ⧫ MOVE SPEED: " + pokemon[i].MoveSpeed, true)
       				.addField("Skills", pokemon[i].Ability)
       				.setImage("https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.pokewiki.de%2Fimages%2Fthumb%2F6%2F6c%2FSugimori_025.png%2F250px-Sugimori_025.png&imgrefurl=https%3A%2F%2Fwww.pokewiki.de%2FPikachu&docid=IoMwWOt4siFKgM&tbnid=4I4v8PsJini_DM%3A&vet=10ahUKEwjTvpXv5PzhAhUOFRQKHT-mAM8QMwg_KAAwAA..i&w=250&h=234&bih=927&biw=958&q=pikachu&ved=0ahUKEwjTvpXv5PzhAhUOFRQKHT-mAM8QMwg_KAAwAA&iact=mrc&uact=8", 2, 2);
-    				*/
-				msg.channel.send(embed);
+    				
+				msg.channel.send(embed);*/
 
 			}
 		
