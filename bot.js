90e611610e62ff28543622ea38b100f79da0d18d
joinity <18332291+joinity@users.noreply.github.com>
2019-05-02 15:20:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,8 +25,10 @@ client.on('message', msg => {
       				.addField("Skills", pokemon[i].Ability)
       				.setImage("https://img.pokemondb.net/sprites/black-white/normal/"+ pkmn +".png")
     				.setThumbnail("https://img.pokemondb.net/sprites/black-white/anim/normal/"+ pkmn +".gif");
-				msg.channel.send({embed}).then(sentMessage => {
-    sentMessage.delete(10000);
+				msg.channel.send({embed}).then(msg => {
+    msg.delete(10000)
+  })
+  .catch(msg.channel.send("error");
 
 			}
 		
