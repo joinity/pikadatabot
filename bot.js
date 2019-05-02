4dd7ec623fc15654d3bafde560c3490c73312e20
joinity <18332291+joinity@users.noreply.github.com>
2019-05-02 19:45:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ client.on('message', msg => {
 					const embed = new Discord.RichEmbed()
 					.setAuthor(pokemon[i].Name, "https://img.pokemondb.net/sprites/black-white/normal/"+ pkmn +".png" )
 					.setColor(0xFF0000)
-					.addField("Stats:", "Level: " + pokemon[i].Level)
+					.addField("Stats:", "Level: " + pokemon[i].Level + "\r\n Type: " + pokemon[i].Type)
 					.addField("\u200B","Attack: " + pokemon[i].Damage + "\r\n HP: " + pokemon[i].Health + "\r\n Defense: " + pokemon[i].Defense + "\r\n Range: " + pokemon[i].Range + "\r\n DPS: " + pokemon[i].DPS, true)
 					.addField("\u200B","Move Speed: " + pokemon[i].MoveSpeed + "\r\n Cost: " + pokemon[i].Cost+ "\r\n Total Cost: " + pokemon[i].TotalCost +"\r\n Catch Rate: " + pokemon[i].CatchRate , true)
 					.addField("Skill:", pokemon[i].Ability) 
