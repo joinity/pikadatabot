8de481d94bc96ca4d4ab77dc7c8c6f6f82c3e0e3
joinity <18332291+joinity@users.noreply.github.com>
2019-05-02 13:43:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,13 +16,13 @@ client.on('message', msg => {
 			
 			if(pkmn == pokemon[i].Name.toLowerCase()){
 				const embed = new Discord.RichEmbed()
-      .setAuthor(pokemon[i], "LVL" + pokemon[i].Level)
-      .setColor(0xFF0000)
-      .addField("Stats", " ATK: " + pokemon[i].Damage + "\r\n ⧫ HP: " + pokemon[i].Health + "\r\n ⧫ DEF: " + pokemon[i].Defense, true)
-      .addField("\u200B", " RANGE: " + pokemon[i].Range+ "\r\n ⧫ DPS: " + pokemon[i].DPS + "\r\n ⧫ MOVE SPEED: " + pokemon[i].MoveSpeed, true)
-      .addField("Skills", pokemon[i].Ability)
-      .setImage("https://lh3.googleusercontent.com/rA0lKRGI_-bP-Jj4nkVc5lm6WJfO3nYlAz089otvQnLeevIoao1CTvaU0l0dqnnWIvLZTSOTaEwj6W04IZSRHQz3NYWiePtJnW3bANh54aI=w120", 2, 2)
-    message.channel.send(embed);
+     				.setAuthor(pokemon[i], "LVL" + pokemon[i].Level)
+      				.setColor(0xFF0000)
+      				.addField("Stats", " ATK: " + pokemon[i].Damage + "\r\n ⧫ HP: " + pokemon[i].Health + "\r\n ⧫ DEF: " + pokemon[i].Defense, true)
+      				.addField("\u200B", " RANGE: " + pokemon[i].Range+ "\r\n ⧫ DPS: " + pokemon[i].DPS + "\r\n ⧫ MOVE SPEED: " + pokemon[i].MoveSpeed, true)
+      				.addField("Skills", pokemon[i].Ability)
+      				.setImage("https://lh3.googleusercontent.com/rA0lKRGI_-bP-Jj4nkVc5lm6WJfO3nYlAz089otvQnLeevIoao1CTvaU0l0dqnnWIvLZTSOTaEwj6W04IZSRHQz3NYWiePtJnW3bANh54aI=w120", 2, 2)
+    				msg.channel.send(embed);
 
 			}
 		
