5ee289ef9041ae9b96cd24dfd929f55101e9980e
joinity <18332291+joinity@users.noreply.github.com>
2019-05-05 16:23:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('message', msg => {
 		for(var i=0;i<pokemon.length;i++){
 
 				if(pkmn == pokemon[i].Name.toLowerCase()){
-					if(pokemon[i].Ability == "") { pokemon[i].Ability = "null";}
+					
 					const embed = new Discord.RichEmbed()
 					.setAuthor(pokemon[i].Name, "https://img.pokemondb.net/sprites/black-white/normal/"+ pkmn +".png" )
 					.setColor(0xFF0000)
@@ -43,13 +43,12 @@ client.on('message', msg => {
 					const embed = new Discord.RichEmbed()
 					.setAuthor(pokemon[i].Name, "https://img.pokemondb.net/sprites/black-white/normal/"+ pkmn +".png" )
 					.setColor(0xFF0000)
-					.addField("Stats", "Level: " + pokemon[i].Level)
-					.addField("\u200B","Attack: \r" + pokemon[i].Damage + "\n HP: " + pokemon[i].Health + "\r\n Defense: " + pokemon[i].Defense, true)
-					.addField("\u200B", "Range: " + pokemon[i].Range+ "\r\n DPS: " + pokemon[i].DPS + "\r\n Move Speed: " + pokemon[i].MoveSpeed, true)
-					.addField("\u200B", "Cost: " + pokemon[i].Cost+ "\r\n Total Cost: " + pokemon[i].TotalCost + "\r\n Catch Rate: " + pokemon[i].CatchRate, true)
-					.addField("Skill:", pokemon[i].Ability)
+					.addField("Stats:", "Level: " + pokemon[i].Level + "\r\n Type: " + pokemon[i].Type)
+					.addField("\u200B","Attack: " + pokemon[i].Damage + "\r\n HP: " + pokemon[i].Health + "\r\n Defense: " + pokemon[i].Defense + "\r\n Range: " + pokemon[i].Range + "\r\n DPS: " + pokemon[i].DPS, true)
+					.addField("\u200B","Move Speed: " + pokemon[i].MoveSpeed + "\r\n Cost: " + pokemon[i].Cost+ "\r\n Total Cost: " + pokemon[i].TotalCost +"\r\n Catch Rate: " + pokemon[i].CatchRate , true)
+					.addField("Skill:", pokemon[i].Ability) 
 					.setThumbnail("https://img.pokemondb.net/sprites/black-white/anim/normal/"+ pkmn +".gif");
-					msg.channel.send({embed});
+					msg.author.send({embed});
 
 				}
 
